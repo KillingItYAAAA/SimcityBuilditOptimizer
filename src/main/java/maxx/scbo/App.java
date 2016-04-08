6980205f249542eec7ee71ee2e15a046e4005749
Peter Horvath <horvath.akos.peter@gmail.com>
2016-04-08 22:48:31 +0200

    We have our first result!

--- src/main/java/maxx/scbo/App.java
+++ src/main/java/maxx/scbo/App.java
@@ -1,14 +1,39 @@
 package maxx.scbo;
 
+import org.apache.commons.math3.optim.MaxIter;
+import org.apache.commons.math3.optim.PointValuePair;
+import org.apache.commons.math3.optim.linear.LinearConstraint;
+import org.apache.commons.math3.optim.linear.LinearConstraintSet;
+import org.apache.commons.math3.optim.linear.LinearObjectiveFunction;
+import org.apache.commons.math3.optim.linear.NonNegativeConstraint;
+import org.apache.commons.math3.optim.linear.Relationship;
+import org.apache.commons.math3.optim.linear.SimplexSolver;
+import org.apache.commons.math3.optim.nonlinear.scalar.GoalType;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+
 /**
- * Hello World.
- * 
  * @author phorvath
  */
 public class App {
   public static void main(String[] args) {
     System.out.println("Hello World!");
+
+    LinearObjectiveFunction l = new LinearObjectiveFunction(new double[] { 1, 1 }, 0);
+
+    Collection<LinearConstraint> constraints = new ArrayList<LinearConstraint>();
+
+    constraints.add(new LinearConstraint(new double[] { 1, 0 }, Relationship.GEQ, 0));
+    constraints.add(new LinearConstraint(new double[] { 0, 1 }, Relationship.GEQ, 0));
+    constraints.add(new LinearConstraint(new double[] { 1, 0 }, Relationship.LEQ, 1));
+    constraints.add(new LinearConstraint(new double[] { 0, 1 }, Relationship.LEQ, 1));
+
+    SimplexSolver solver = new SimplexSolver();
+    PointValuePair optSolution = solver.optimize(new MaxIter(100), l, new LinearConstraintSet(constraints),
+        GoalType.MAXIMIZE, new NonNegativeConstraint(true));
     
-    ExLinearObjectiveFunction l = new ExLinearObjectiveFunction(5,0);
+    System.out.println("Solution:\nValue: "+optSolution.getValue()+" at "+Arrays.toString(optSolution.getKey()));
   }
 }
