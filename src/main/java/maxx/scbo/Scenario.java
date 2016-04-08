7854338eaf03082331811e846b35fef581521211
Peter Horvath <horvath.akos.peter@gmail.com>
2016-04-08 23:00:14 +0200

    Started a minimal Scenario class

---
+++ src/main/java/maxx/scbo/Scenario.java
@@ -0,0 +1,17 @@
+package maxx.scbo;
+
+/**
+ * @author phorvath
+ *
+ */
+public class Scenario {
+  private int nFactory;
+
+  public int getNFactory() {
+    return nFactory;
+  }
+
+  public void setNFactory(int nFactory) {
+    this.nFactory = nFactory;
+  }
+}
