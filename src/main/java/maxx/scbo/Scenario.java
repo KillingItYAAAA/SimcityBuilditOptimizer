50efef27ee7617eec9ddefc427c4089b139e9ac5
Peter Horvath <horvath.akos.peter@gmail.com>
2016-04-09 00:27:59 +0200

    Scenario, modell, resources start

--- src/main/java/maxx/scbo/Scenario.java
+++ src/main/java/maxx/scbo/Scenario.java
@@ -1,19 +1,21 @@
 package maxx.scbo;
 
+import org.apache.commons.math3.optim.linear.LinearConstraint;
+
+import java.util.LinkedList;
+
 /**
  * @author phorvath
  *
  */
 public class Scenario {
-  private int nFactory;
-
-  public int getNFactory() {
-    return nFactory;
-  }
-
-  public void setNFactory(int nFactory) {
-    this.nFactory = nFactory;
+  private LinkedList<Producer> producers;
+  
+  public void addProducer(Producer producer) {
+    producers.add(producer);
   }
   
-  
+  public LinkedList<LinearConstraint> getConstraints() {
+    return new LinkedList<LinearConstraint>();
+  }
 }
