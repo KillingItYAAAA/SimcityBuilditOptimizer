127744582311618b29eb800eccccb54d9c4224f9
Peter Horvath <horvath.akos.peter@gmail.com>
2016-04-12 22:10:09 +0200

    RawRelation moved out, it was too big for a single file

--- src/main/java/maxx/scbo/ConfigLoader.java
+++ src/main/java/maxx/scbo/ConfigLoader.java
@@ -13,53 +13,6 @@ import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 
 public class ConfigLoader {
-  private class RawRelation implements Comparable<RawRelation> {
-    private Id id = new Id();
-    
-    private String parent;
-    private String child;
-    private int no;
-
-    RawRelation(String parent, String child, int no) {
-      setParent(parent);
-      setChild(child);
-      setNo(no);
-    }
-
-    public String getParent() {
-      return parent;
-    }
-
-    public void setParent(String parent) {
-      this.parent = parent;
-    }
-
-    public String getChild() {
-      return child;
-    }
-
-    public void setChild(String child) {
-      this.child = child;
-    }
-
-    public int getNo() {
-      return no;
-    }
-
-    public void setNo(int no) {
-      this.no = no;
-    }
-    
-    public Integer getId() {
-      return id.getId();
-    }
-    
-    @Override
-    public int compareTo(RawRelation rawRelation) {
-      return rawRelation.getId().compareTo(getId());
-    }
-  }
-
   private TreeSet<RawRelation> rawRelations = new TreeSet<RawRelation>();
 
   private class ConfigHandler extends DefaultHandler {
